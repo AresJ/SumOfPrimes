f0002ce80aaef6934ae9e4496355a488ee9a8fd9
are$ <jaedagk@live.com>
2015-06-21 10:46:45 -0400

    First Commit

---
+++ Sum of Primes.cpp
@@ -0,0 +1,44 @@
+//
+//  Sum of Primes.cpp
+//  
+//
+//  Created by are$ on 6/21/15.
+//
+//
+
+#include "Sum of Primes.h"
+#include <iostream>
+
+bool primes(number);
+
+int main(){
+    int sum = 0; // The Sum
+    int number = 0; // The Number
+    int numberOfTimes = 1000; //The Number of Primes Needing To Summed
+    int a = 2; //Count
+    
+    while (number < numberOfTimes){
+        if(primes(number)){
+            sum += a;
+            ++number;
+                }
+            }
+        ++a;
+    }
+
+cout << endl << sum << endl;
+
+return 0;
+
+}
+
+bool primes(int number){
+    for(int a = 2; a < number; a++){
+        if(number % a == 0){
+            return false;
+        }
+    }
+    
+    return true;
+    
+}
---
+++ Sum of Primes.h
@@ -0,0 +1,14 @@
+//
+//  Sum of Primes.h
+//  
+//
+//  Created by are$ on 6/21/15.
+//
+//
+
+#ifndef ____Sum_of_Primes__
+#define ____Sum_of_Primes__
+
+#include <stdio.h>
+
+#endif /* defined(____Sum_of_Primes__) */
